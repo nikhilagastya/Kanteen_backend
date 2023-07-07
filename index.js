4a3657fdda50d6c6f2d25dd06e2f5b4e12f6e08b
nikhilagastya <72994295+nikhilagastya@users.noreply.github.com>
2023-07-08 02:06:47 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT ||5000;
 const mongo = require("./db");
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.setHeader("Access-Control-Allow-Origin", "*");
   res.header(
     "Access-Control-Allow-Headers",
     "Origin,X-Request-With,Content-Type,Accept"
